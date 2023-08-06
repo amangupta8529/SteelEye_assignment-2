8d80d4eda405a01588d63fdb150b022ab40a5efa
Aman Kumar Gupta <87220284+amangupta8529@users.noreply.github.com>
2023-08-06 19:25:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 function highlightHTMLContent(htmlContent, plainText, plainTextPositions) {
-    let offset = 0;
-    const markedText = plainTextPositions.reduce((highlightedText, { start, end }) => {
-        const plainWord = plainText.slice(start, end);
-        const htmlStart = start + offset;
-        const htmlEnd = start + offset + plainWord.length;
-        const htmlWord = htmlContent.slice(htmlStart, htmlEnd);
-        const markedWord = `<mark>${htmlWord}</mark>`;
+    plainTextPositions.sort((a, b) => b.start - a.start);
+    for (const position of plainTextPositions) {
+        const startTag = '<mark>';
+        const endTag = '</mark>';
+        const start = htmlContent.indexOf(plainText.substr(position.start, position.end - position.start));
+        const end = start + (position.end - position.start);
+        htmlContent = htmlContent.slice(0, start) + startTag + htmlContent.slice(start, end) + endTag + htmlContent.slice(end);
+    }
 
-        // Update the offset for HTML tags
-        const tagsInPlainText = plainText.slice(start).match(/<[^>]*>/g);
-        const tagsInHtmlContent = htmlContent.slice(htmlStart).match(/<[^>]*>/g);
-        if (tagsInPlainText && tagsInHtmlContent) {
-            const tagCountDifference = tagsInHtmlContent.length - tagsInPlainText.length;
-            offset += tagCountDifference * 3; // Each tag takes 3 characters (<mark> or </mark>)
-        }
-
-        return highlightedText.slice(0, htmlStart) + markedWord + highlightedText.slice(htmlEnd);
-    }, htmlContent);
-
-    return markedText;
+    return htmlContent;
 }
 
 module.exports = highlightHTMLContent;
